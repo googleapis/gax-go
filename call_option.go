64e4afeb7e168216826d50ac141994b42ed4209d
Michael Darakananda <pongad@google.com>
2016-08-22 12:10:03 +1000

    Use a function to decide whether we should retry
    This allows users to use arbitrarily sophisticated retry handling
    mechanism.

--- call_option.go
+++ call_option.go
@@ -1,8 +1,39 @@
+// Copyright 2016, Google Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package gax
 
 import (
+	"math/rand"
 	"time"
 
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
 
@@ -10,109 +41,76 @@ type CallOption interface {
 	Resolve(*CallSettings)
 }
 
-type callOptions []CallOption
+type retryerOption func() Retryer
 
-func (opts callOptions) Resolve(s *CallSettings) *CallSettings {
-	for _, opt := range opts {
-		opt.Resolve(s)
-	}
-	return s
+func (o retryerOption) Resolve(s *CallSettings) {
+	s.Retryer = o
 }
 
-// Encapsulates the call settings for a particular API call.
+func WithBackoffRetryer(base, max time.Duration, mult float64, codes ...codes.Code) CallOption {
+	if len(codes) == 0 {
+		return retryerOption(nil)
+	}
+	return retryerOption(func() Retryer {
+		return &codeRetryer{
+			b: &exponentialBackoff{Base: base, Max: max, Mult: mult},
+			c: codes,
+		}
+	})
+}
+
 type CallSettings struct {
-	Timeout       time.Duration
-	RetrySettings RetrySettings
+	// Retryer returns a Retryer to be used to control retry logic of a method call.
+	// If retry is undesirable, simply set the function to nil.
+	// If the function is not nil, it must return non-nil Retryer.
+	Retryer func() Retryer
 }
 
-// Per-call configurable settings for retrying upon transient failure.
-type RetrySettings struct {
-	RetryCodes      map[codes.Code]bool
-	BackoffSettings BackoffSettings
+type Retryer interface {
+	// Retry decides whether a request should be retried and how long to wait before retrying
+	// if the previous attempt returned with err. err is never nil.
+	Retry(err error) (backoff time.Duration, ok bool)
 }
 
-// Parameters to the exponential backoff algorithm for retrying.
-type BackoffSettings struct {
-	DelayTimeoutSettings MultipliableDuration
-	RPCTimeoutSettings   MultipliableDuration
+type BackoffStrategy interface {
+	Pause() (time.Duration, bool)
 }
 
-type MultipliableDuration struct {
-	Initial    time.Duration
-	Max        time.Duration
-	Multiplier float64
+type codeRetryer struct {
+	b BackoffStrategy
+	c []codes.Code
 }
 
-func (w CallSettings) Resolve(s *CallSettings) {
-	s.Timeout = w.Timeout
-	s.RetrySettings = w.RetrySettings
-
-	s.RetrySettings.RetryCodes = make(map[codes.Code]bool, len(w.RetrySettings.RetryCodes))
-	for key, value := range w.RetrySettings.RetryCodes {
-		s.RetrySettings.RetryCodes[key] = value
+func (r *codeRetryer) Retry(err error) (time.Duration, bool) {
+	c := grpc.Code(err)
+	for _, rc := range r.c {
+		if rc == c {
+			return r.b.Pause()
+		}
 	}
+	return 0, false
 }
 
-type withTimeout time.Duration
+// exponentialBackoff implements exponential backoff.
+// It is similar to gensupport.ExponentialBackoff,
+// but allows arbitrary Mult
+// and retries forever within Max.
+type exponentialBackoff struct {
+	Base time.Duration
+	Max  time.Duration
+	Mult float64
 
-func (w withTimeout) Resolve(s *CallSettings) {
-	s.Timeout = time.Duration(w)
+	d time.Duration
 }
 
-// WithTimeout sets the client-side timeout for API calls if the call isn't
-// retrying.
-func WithTimeout(timeout time.Duration) CallOption {
-	return withTimeout(timeout)
-}
-
-type withRetryCodes []codes.Code
-
-func (w withRetryCodes) Resolve(s *CallSettings) {
-	s.RetrySettings.RetryCodes = make(map[codes.Code]bool)
-	for _, code := range []codes.Code(w) {
-		s.RetrySettings.RetryCodes[code] = true
+func (b *exponentialBackoff) Pause() (time.Duration, bool) {
+	if b.d < b.Base {
+		b.d = b.Base
 	}
-}
-
-// WithRetryCodes sets a list of Google API canonical error codes upon which a
-// retry should be attempted. If nil, the call will not retry.
-func WithRetryCodes(retryCodes []codes.Code) CallOption {
-	return withRetryCodes(retryCodes)
-}
-
-type withDelayTimeoutSettings MultipliableDuration
-
-func (w withDelayTimeoutSettings) Resolve(s *CallSettings) {
-	s.RetrySettings.BackoffSettings.DelayTimeoutSettings = MultipliableDuration(w)
-}
-
-// WithDelayTimeoutSettings specifies:
-// - The initial delay time, in milliseconds, between the completion of
-//   the first failed request and the initiation of the first retrying
-//   request.
-// - The multiplier by which to increase the delay time between the
-//   completion of failed requests, and the initiation of the subsequent
-//   retrying request.
-// - The maximum delay time, in milliseconds, between requests. When this
-//   value is reached, `RetryDelayMultiplier` will no longer be used to
-//   increase delay time.
-func WithDelayTimeoutSettings(initial time.Duration, max time.Duration, multiplier float64) CallOption {
-	return withDelayTimeoutSettings(MultipliableDuration{initial, max, multiplier})
-}
-
-type withRPCTimeoutSettings MultipliableDuration
-
-func (w withRPCTimeoutSettings) Resolve(s *CallSettings) {
-	s.RetrySettings.BackoffSettings.RPCTimeoutSettings = MultipliableDuration(w)
-}
-
-// WithRPCTimeoutSettings specifies:
-// - The initial timeout parameter to the request.
-// - The multiplier by which to increase the timeout parameter between
-//   failed requests.
-// - The maximum timeout parameter, in milliseconds, for a request. When
-//   this value is reached, `RPCTimeoutMultiplier` will no longer be used
-//   to increase the timeout.
-func WithRPCTimeoutSettings(initial time.Duration, max time.Duration, multiplier float64) CallOption {
-	return withRPCTimeoutSettings(MultipliableDuration{initial, max, multiplier})
+	d := b.d
+	b.d = time.Duration(float64(b.d) * b.Mult)
+	if b.d > b.Max {
+		b.d = b.Max
+	}
+	return time.Duration(rand.Int63n(int64(d))), true
 }
--- call_option_test.go
+++ call_option_test.go
@@ -1,47 +1,76 @@
+// Copyright 2016, Google Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package gax
 
 import (
-	"reflect"
 	"testing"
 	"time"
 
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
 
-func TestCallOptions(t *testing.T) {
-	expected := &CallSettings{
-		time.Second * 1,
-		RetrySettings{
-			map[codes.Code]bool{codes.Unavailable: true, codes.DeadlineExceeded: true},
-			BackoffSettings{
-				MultipliableDuration{time.Second * 2, time.Second * 4, 3.0},
-				MultipliableDuration{time.Second * 5, time.Second * 7, 6.0},
-			},
-		},
+func TestExponentialBackoff(t *testing.T) {
+	eb := exponentialBackoff{Base: 1, Max: 20, Mult: 2}
+	want := []time.Duration{1, 2, 4, 8, 16, 20, 20, 20, 20, 20}
+	for _, w := range want {
+		if d, ok := eb.Pause(); !ok {
+			t.Error("exponentialBackoff should keep pausing forever")
+		} else if d > w {
+			t.Errorf("Backoff duration should be at most %s, got %s", w, d)
+		}
 	}
+}
 
-	settings := &CallSettings{}
-	opts := []CallOption{
-		WithTimeout(time.Second * 1),
-		WithRetryCodes([]codes.Code{codes.Unavailable, codes.DeadlineExceeded}),
-		WithDelayTimeoutSettings(time.Second*2, time.Second*4, 3.0),
-		WithRPCTimeoutSettings(time.Second*5, time.Second*7, 6.0),
+func TestBackoffRetryer(t *testing.T) {
+	apiErr := grpc.Errorf(codes.Unavailable, "")
+	tests := []struct {
+		c     []codes.Code
+		retry bool
+	}{
+		{nil, false},
+		{[]codes.Code{codes.DeadlineExceeded}, false},
+		{[]codes.Code{codes.DeadlineExceeded, codes.Unavailable}, true},
+		{[]codes.Code{codes.Unavailable}, true},
 	}
-	callOptions(opts).Resolve(settings)
+	for _, tst := range tests {
+		var settings CallSettings
+		WithBackoffRetryer(1, 2, 1.2, tst.c...).Resolve(&settings)
 
-	if !reflect.DeepEqual(settings, expected) {
-		t.Errorf("piece-by-piece settings don't match their expected configuration")
-	}
+		var retry bool
+		if settings.Retryer != nil {
+			_, retry = settings.Retryer().Retry(apiErr)
+		}
 
-	settings = &CallSettings{}
-	expected.Resolve(settings)
-
-	if !reflect.DeepEqual(settings, expected) {
-		t.Errorf("whole settings don't match their expected configuration")
-	}
-
-	expected.RetrySettings.RetryCodes[codes.FailedPrecondition] = true
-	if _, ok := settings.RetrySettings.RetryCodes[codes.FailedPrecondition]; ok {
-		t.Errorf("unexpected modification in the RetryCodes map")
+		if retry != tst.retry {
+			t.Errorf("retryable codes: %v, error code: %s, retry: %t, want %t", tst.c, grpc.Code(apiErr), retry, tst.retry)
+		}
 	}
 }
