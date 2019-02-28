e588366d338bbf1a3a240520aa720a0c64f2a64e
Jean de Klerk <deklerk@google.com>
2019-02-28 11:38:29 -0700

    internal: update CI/CD scripts
    - Use go mod download to download deps instead of go get.
    - Use GO111MODULE=on everywhere applicable.
    - Switch to being 1.12 aware.
    - Add tools.go to codify CI/CD dependencies.
    - Allows breaking changes to occur, similar to https://github.com/google/go-genproto/commit/035c5b9201420e8b5ab4e32e945b5c48a9bcaccf
    
    BREAKING_CHANGE_ACCEPTABLE=version changing

--- internal/kokoro/test.sh
+++ internal/kokoro/test.sh
@@ -15,7 +15,7 @@ go version
 
 # Set $GOPATH
 export GOPATH="$HOME/go"
-export GAX_HOME=$GOPATH/src/googleapis/gax-go
+export GAX_HOME=$GOPATH/src/github.com/googleapis/gax-go
 export PATH="$GOPATH/bin:$PATH"
 mkdir -p $GAX_HOME
 
@@ -24,10 +24,24 @@ git clone . $GAX_HOME
 cd $GAX_HOME
 
 try3() { eval "$*" || eval "$*" || eval "$*"; }
-try3 go get -v -t ./...
 
-./internal/kokoro/vet.sh
+download_deps() {
+    if [[ `go version` == *"go1.11"* ]] || [[ `go version` == *"go1.12"* ]]; then
+        export GO111MODULE=on
+        # All packages, including +build tools, are fetched.
+        try3 go mod download
+    else
+        # Because we don't provide -tags tools, the +build tools
+        # dependencies aren't fetched.
+        try3 go get -v -t ./...
+    fi
+}
+
+download_deps
 ./internal/kokoro/check_incompat_changes.sh
+./internal/kokoro/vet.sh
+go test -race -v . 2>&1 | tee $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt
 
-# Run tests and tee output to log file, to be pushed to GCS as artifact.
-go test -race -v ./... 2>&1 | tee $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt
+cd v2
+download_deps
+go test -race -v . 2>&1 | tee $KOKORO_ARTIFACTS_DIR/$KOKORO_GERRIT_CHANGE_NUMBER.txt
