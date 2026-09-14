a1bcf7da996037bf889493069b8c9c409603efc7
Seth Hollyman <shollyman@google.com>
2026-09-14 17:43:56 +0000

    testing: refine vet checks
    This PR refines out vet checks.  Namely, we move our staticcheck usage
    to the earliest version, eliminate golint as its been deprecated, and
    remove directives around VCS tagging.

--- internal/kokoro/vet.sh
+++ internal/kokoro/vet.sh
@@ -10,25 +10,30 @@ if [[ $KOKORO_JOB_NAME != *"latest-version"* ]]; then
   exit 0
 fi
 
-# Look at all .go files (ignoring .pb.go files) and make sure they have a Copyright. Fail if any don't.
-find . -type f -name "*.go" ! -name "*.pb.go" -exec grep -L "\(Copyright [0-9]\{4,\}\)" {} \; 2>&1 | tee /dev/stderr | (! read)
 
-# Easier to debug CI.
-pwd
+# In our latest version, do basic checks and module staleness checks.
+if [[ $KOKORO_JOB_NAME == *"latest-version"* ]]; then
 
-# Disable VCS stamping to prevent dubious ownership errors in Kokoro's Docker container
-export GOFLAGS="-buildvcs=false"
+  # Look at all .go files (ignoring .pb.go files) and make sure they have a Copyright. Fail if any don't.
+  find . -type f -name "*.go" ! -name "*.pb.go" -exec grep -L "\(Copyright [0-9]\{4,\}\)" {} \; 2>&1 | tee /dev/stderr | (! read)
 
-# Execute tidy and linters inside every submodule (e.g. v2/)
-# This ensures that we don't just lint the root module.
-# It also fails if a dependency was added without the necessary go.mod/go.sum change.
-for mod in $(find . -name go.mod); do
-  pushd $(dirname $mod)
-  go mod tidy -diff
-  gofmt -s -d -l . 2>&1 | tee /dev/stderr | (! read)
-  goimports -l . 2>&1 | tee /dev/stderr | (! read)
+  # Easier to debug CI.
+  pwd
 
-  golint ./... 2>&1 | tee /dev/stderr | (! read)
-  staticcheck ./...
-  popd
-done
+  # Execute tidy and linters inside every submodule (e.g. v2/)
+  # This ensures that we don't just lint the root module.
+  # It also fails if a dependency was added without the necessary go.mod/go.sum change.
+  for mod in $(find . -name go.mod); do
+    pushd $(dirname $mod)
+    go mod tidy -diff
+    gofmt -s -d -l . 2>&1 | tee /dev/stderr | (! read)
+    goimports -l . 2>&1 | tee /dev/stderr | (! read)
+    popd
+  done
+fi
+
+
+# In our earliest version, do static checking
+if [[ $KOKORO_JOB_NAME == *"latest-version"* ]]; then
+  staticcheck ./... 
+fi
